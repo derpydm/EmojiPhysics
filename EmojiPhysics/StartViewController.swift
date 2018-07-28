e83f8ddc5303172d845e5a59b8d13cec54614e23
derpydm <derpydm@gmail.com>
2018-07-28 10:08:12 +0800

    Created and animated StartViewController

---
+++ EmojiPhysics/StartViewController.swift
@@ -0,0 +1,44 @@
+//
+//  StartViewController.swift
+//  EmojiPhysics
+//
+//  Created by Sean on 28/7/18.
+//  Copyright © 2018 Sean. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    @IBOutlet weak var startLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func startButtonPressed(_ sender: Any) {
+        let animator = UIViewPropertyAnimator(duration: 0.3, curve: .easeInOut) {
+            self.startLabel.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+        }
+        animator.startAnimation()
+        animator.addCompletion { (_) in
+            self.performSegue(withIdentifier: "emojiSegue", sender: Any.self)
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
